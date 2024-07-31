8dbc4630f4a2be96a5dc1519b65d75c2e55022c1
DenisSavostenko0858 <denyotube0858@gmail.com>
2024-07-31 19:03:33 +0300

    completion of work

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-## NestJS + Vite ReactTS + PostgreSQL + Redis + Sequelize
+### Nest + Vite ReactTS + PostgreSQL + Redis + Sequelize
+
+## Jest тестирование
+npm run test
+
+## Nest 
+npm run start
+
+## Vite React 
+npm run dev
+
+## Api для работы с Postman
+
+# Статьи
+- Добавление статьи
+POST: http://localhost:5000/api/news/create
+
+ -Пример запроса JSON:
+{
+    "name": "Статья",
+    "description": "Описание статьи",
+    "published": "2024-06-30T12:20:00Z",
+    "authorId": 1
+}
+
+- Получение всех статей
+GET: http://localhost:5000/api/news/list
+
+- Получение статей по автору
+GET: http://localhost:5000/api/news/list/author/:authorId
+
+- Получение статей по названию статьи
+GET: http://localhost:5000/api/news/list/name/:name
+
+- Получение статьи по id
+GET: http://localhost:5000/api/news/:id
+
+- Изменение статьи по id
+PATCH: http://localhost:5000/api/news/:id
+ 
+ -Пример запроса JSON:
+{
+    "name": "Статья2",
+    "description": "Описание статьи",
+    "published": "2024-06-30T12:20:00Z",
+    "authorId": 1
+}
+
+- Удаление статьи по id
+DELETE: http://localhost:5000/api/news/:id
+
+# Пользователи
+- Register
+POST: http://localhost:5000/api/users/register
+
+ -Пример запроса JSON:
+{
+    "name": "user",
+    "email": "user2@gmail.com",
+    "password": "123123"
+}
+ -Ответ JSON:
+{
+    "token": "eyJhbGciOi...."   
+}
+
+- Login
+POST: http://localhost:5000/api/users/login
+
+ -Пример запроса JSON:
+{
+    "email": "user1@gmail.com",
+    "password": "123123"
+}
+ -Ответ JSON:
+{
+    "token": "eyJhbGciOi...."   
+}
+
+- Auth (проверка токена)
+POST: http://localhost:5000/api/users/auth
+
+ -Пример запроса JSON:
+{
+    "id": "1",
+    "email": "user1@gmail.com",
+    "role": "user"
+}
+ -Ответ JSON:
+{
+    "token": "eyJhbGciOi...."   
+}
--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,24 +1,100 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
+import { NotFoundException } from '@nestjs/common';
+import { NewsService } from '../src/news/news.service';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('AppController', () => {
+  let appController: AppController;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    appController = app.get<AppController>(AppController);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Сервер запущен!');
+    });
+  });
+});
+
+describe('NewsService', () => {
+  let service: NewsService;
+  let mockNewsRepository = {
+    create: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    remove: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        NewsService,
+        {
+          provide: 'NewsRepository',
+          useValue: mockNewsRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<NewsService>(NewsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should create news', async () => {
+    const date = new Date();
+
+  // Название должно быть уникальное, так как есть проверка на плагиат
+    const newsData = {  name: 'Тест', description: 'Тест', published: date, authorId: 1};
+    
+    mockNewsRepository.create.mockReturnValue(newsData);
+    mockNewsRepository.save.mockReturnValue(newsData);
+
+    const result = await service.create(newsData);
+    expect(result).toEqual(newsData);
+    expect(mockNewsRepository.create).toHaveBeenCalledWith(newsData);
+    expect(mockNewsRepository.save).toHaveBeenCalled();
+  });
+
+  it('should return news by id', async () => {
+    const news = { 
+      id: 4, 
+      authorId: 2, 
+      description: 'Ограбление на лен шосе 27', 
+      name: "Новость 3", 
+      published: new Date('2024-06-30T12:20:00.000Z')
+    };
+
+    const fullNews = { ...news, createdAt: new Date(), updatedAt: new Date() };
+
+    mockNewsRepository.findOne.mockReturnValue(fullNews);
+
+    const result = await service.findOne(4);
+    expect(result).toEqual(expect.objectContaining(news));
+  });
+
+  it('should throw an error if news not found', async () => {
+    mockNewsRepository.findOne.mockReturnValue(null);
+  
+    await expect(service.findOne(1)).rejects.toThrow(NotFoundException);
+  });
+
+  it('should delete news', async () => {
+    // Отрабатывает проверка на нахождение новости, она должна быть иначе возвращает ошибку
+    const news = { id: 1};
+    mockNewsRepository.findOne.mockReturnValue(news);
+    mockNewsRepository.remove.mockReturnValue(news);
+
+    await service.remove(1);
+    expect(mockNewsRepository.remove).toHaveBeenCalledWith(news);
   });
 });
